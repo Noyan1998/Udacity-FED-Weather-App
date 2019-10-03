9011ff8731cd04a4cd76034c54ee2ca124ccf5a3
conordewey3 <cdewey96@vt.edu>
2019-10-02 20:34:04 -0400

    middleware and set up next steps

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
-const bosyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -14,9 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, function () {
+    console.log(`running on localhost: ${port}`);
+});
+
+// GET route that returns projectData
+
+// POST route adding data to ProjectData
+

9dd8752588490b1c6f2c8cb4013edce4f87dd4cf
conordewey3 <cdewey96@vt.edu>
2019-10-03 16:12:46 -0400

    api keys and url

--- website/app.js
+++ website/app.js
@@ -1,7 +1,6 @@
-/* Global Variables */
-
 // Personal API Key for OpenWeatherMap API
-// 1a09117c381d7902dc386c387ce1ec07
+const openWeatherKey = '1a09117c381d7902dc386c387ce1ec07';
+const baseURL = 'api.openweathermap.org/data/2.5/weather?';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -14,5 +13,30 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 /* Function to GET Web API Data */
 
 /* Function to POST data */
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch(error) {
+        console.log('error', error);
+    };
+}
 
 /* Function to GET Project Data */
+const retrieveData = async(url='') => {
+    const request = await fetch(url);
+    try {
+        const allData = await request.json();
+    } catch(error) {
+        console.log('error', error);
+    };
+};
